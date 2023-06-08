e211d0dab53004001cb41ee3709d63bc97eab293
Shrabon Ahamed <shrabonahamed106@gmail.com>
2023-06-09 02:00:55 +0600

    added navbar items

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,39 +1,34 @@
+import Link from "next/link";
 
 const Navbar = () => {
+
+    const menuItems = <>
+        <li><Link className="px-3" href='/'>Home</Link></li>
+        <li><Link className="px-3" href='/About'>About</Link></li>
+        <li><Link className="px-3" href='/Services'>Services</Link></li>
+        <li><Link className="px-3" href='/Services'>Contact</Link></li>
+        <li><Link className="px-3" href='/Login'>Register</Link></li>
+        <li><Link className="px-3" href='/Login'>Login</Link></li>
+
+    </>
+
+
     return (
-        <div className="navbar bg-base-300">
+        <div className="navbar bg-base-300 " >
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <ul tabIndex={0} className="menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <Link href='/' className="text-xl flex justify-end">Web-Solutions-IT</Link>
             </div>
             <div className="navbar-end hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                <ul className=" menu-horizontal px-1">
+                    {menuItems}
                 </ul>
             </div>
 
